21fc9d77a3b2eb0985ccd2df542e36a91bf7b171
lekejosh <lekejosh6wf@gmail.com>
2022-03-23 01:22:53 +0100

    JSX variable

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   //JSX method
   var style1 = {
     color: "blue",
     fontSize: 30,
-    backgroundColor:"yellow"
-  }
-
-  return(
+    backgroundColor: "yellow",
+  };
+  var var1 = "Hello WOrld";
+  var var2 = 12 / 1;
+  var var3 = 33 / 32;
+  return (
     <div>
-     <h1 style={style1} title="This is the heading">Heading -JSX Method</h1>
-     <p id="para1">This is the paragraph</p>
-     <p className="para2">Second paragraph</p>
-     <div>
-       <label htmlFor="input1">Data</label> 
-       <br ></br>
-       <input id="input1" name="input1"></input>
-     </div>
+      <h1 style={style1} title="This is the heading">
+        Heading -JSX Method
+      </h1>
+      <p id="para1">This is the paragraph</p>
+      <p className="para2">Second paragraph</p>
+      <p>
+        {var1 + var2} {var3}
+      </p>
+      <p>{var2 === 6 ? "True" : "False"}</p>
+
+      <div>
+        <label htmlFor="input1">Data</label>
+        <br></br>
+        <input id="input1" name="input1"></input>
+      </div>
     </div>
-  )
+  );
 
-
-//Main
+  //Main
 
   // return(
   //   <div>
@@ -31,10 +39,9 @@ function App() {
   //   </div>
   // )
 
-
-// Alternatively
+  // Alternatively
 
   // return React.createElement("h1", {title: "this is the Heading"}, "heading")
 }
 
-export default App; 
+export default App;
